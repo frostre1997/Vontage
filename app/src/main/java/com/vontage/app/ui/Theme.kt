3dcd5bc47000a51b2a36c6ea1f26e5aaf6465fa7
Frostre_ <n9043395@gmail.com>
2026-07-23 15:32:00 +0200

    Create Theme.kt

---
+++ app/src/main/java/com/vontage/app/ui/Theme.kt
@@ -0,0 +1,9 @@
+package com.vontage.app.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun VontageTheme(content: @Composable () -> Unit) {
+    MaterialTheme(content = content)
+}
